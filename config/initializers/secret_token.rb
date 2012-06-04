72db18b56b620d181bfccd4b8a44baa3a9cb3e94
Rory McCune <rory.mccune@scotsts.com>
2012-06-04 17:24:27 +0100

    Initial Commit before doing anything

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Srctwit::Application.config.secret_token = '7e6d3d90c233db22788b149b415d7a4f9a2bb891ecd80956af45769a78dbf1534dc251f243f0dab7d890392c3c96671744dd77c96f0560972734d17b5a6e4291'
